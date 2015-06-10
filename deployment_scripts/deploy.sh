e85a62a1cc190d59166102abfd38dba5d0557704
Igor Gajsin <igajsin@mirantis.com>
2015-06-10 18:34:01 +0300

    add the task install the driver
    Change-Id: I5a63c75c598d91c397d3881b678a8e5d23cdd241

--- deployment_scripts/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# It's a script which deploys your plugin
-echo fuel-plugin-vmware-dvs > /tmp/fuel-plugin-vmware-dvs
---
+++ deployment_scripts/install_the_driver.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Install the driver.
+pip install git+git://github.com/Mirantis/vmware-dvs.git@mos-6.1
