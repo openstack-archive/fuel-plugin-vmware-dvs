276d7e758f1bb6da0d9a9840483814ad257b651a
Igor Gajsin <igajsin@mirantis.com>
2015-06-11 14:35:36 +0300

    task have to apply on primary-controller
    Change-Id: Ie23f78e3c45dace28fd92a019100592235728171

--- deployment_scripts/install_the_driver.sh
+++ deployment_scripts/install_the_driver.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# notice
+echo Start setup vmware-dvs neutron ml2 plugin > /tmp/fuel-plugin-vmware-dvs.log
+
 # Install the driver.
 pip install git+git://github.com/Mirantis/vmware-dvs.git@mos-6.1
