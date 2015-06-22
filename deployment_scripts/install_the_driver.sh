7e858baa0e6109a4acc0452fb11d0854b289ee1a
Igor Gajsin <igajsin@mirantis.com>
2015-06-22 17:06:40 +0300

    The plugins's prototype ready to qa
    That's only prototype not ready to use plugin. It devoid a lot of
    things and has only mock instead puppet's manifest. But it enough for
    installation and deployment. QA can start checking.
    
    Change-Id: I93cf2025cc5a9477443bdc97e3dc0da6166db972

--- deployment_scripts/install_the_driver.sh
+++ deployment_scripts/install_the_driver.sh
@@ -6,7 +6,58 @@ plugin_name=fuel-plugin-vmware-dvs
 plugin_version=1.0
 ip=`hiera master_ip`
 port=8080
+repo=simple
 
-url=http://$ip:$port/plugins/$plugin_name-$plugin_version/vmware-dvs
+function _nova_patch {
+    wget -O /usr/lib/python2.7/dist-packages/nova.patch "http://$ip:$port/plugins/$plugin_name-$plugin_version/nova.patch" && cd /usr/lib/python2.7/dist-packages/ ; patch -p1 < nova.patch
+    for r in $(crm_mon -1|awk '/nova_compute_vmware/ {print $1}'); do
+	crm resource restart $r
+    done
+}
 
-pip install $url
+function _dirty_hack {
+    cd /usr/lib/python2.7/dist-packages/oslo
+    mv messaging messaging.old
+    cd /usr/lib/python2.7/dist-packages/
+    mv suds suds.old
+}
+
+function _neutron_restart {
+    service neutron-server restart
+}
+
+function _core_install {
+    easy_install pip
+    apt-get -y install git-core python-dev
+}
+
+function _driver_install {
+    cd /usr/local/lib/python2.7/dist-packages/
+    pip install -e git+git://github.com/yunesj/suds#egg=suds
+    pip install oslo.messaging==1.8.3
+    pip install git+git://github.com/Mirantis/vmware-dvs.git@mos-6.1
+}
+
+function _ln {
+    cd /usr/local/lib/python2.7/dist-packages/oslo
+    ln -s /usr/lib/python2.7/dist-packages/oslo/db
+    ln -s /usr/lib/python2.7/dist-packages/oslo/rootwrap
+}
+
+function _config {
+    cd /etc/neutron
+    cp neutron.conf neutron.conf.old
+    sed -i  s/"#notification_driver.*"/notification_driver=messagingv2/ neutron.conf
+    sed -i  s/"#notification_topics.*"/notification_topics=vmware_dvs/ neutron.conf
+    cd /etc/neutron/plugins/ml2
+    mv ml2_conf.ini ml2_conf.ini.old
+    wget "http://$ip:$port/plugins/$plugin_name-$plugin_version/ml2_conf.ini"
+}
+
+_nova_patch
+_core_install
+_dirty_hack
+_driver_install
+_ln
+_config
+_neutron_restart
