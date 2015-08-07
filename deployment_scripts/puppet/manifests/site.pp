c13ca37776e1890797a2ef64412627c1a8ff9ed9
Igor Gajsin <igajsin@mirantis.com>
2015-08-07 12:54:04 +0300

    recreate the net04 and net04_ext networks
    There is one peculiarity of the vmware_dvs driver: it can't works with
    the net04 and net04_ext neutron networks cause it have been created
    before driver had installed.
    
    To make it works properly this networks should be deleted and was
    created again.
    
    Change-Id: I8c83ef88cc8ad8d4fd4cf1d330c47c390362a78d

--- deployment_scripts/puppet/manifests/site.pp
+++ deployment_scripts/puppet/manifests/site.pp
@@ -14,20 +14,27 @@
 
 notice('MODULAR: fuel-plugin-vmware-dvs')
 
-$vc_hash           = hiera('vcenter', {})
-$dvs_hash          = hiera('fuel-plugin-vmware-dvs', {})
-$neutron_hash      = hiera('quantum_settings', {})
-$vsphere_hostname  = inline_template('<%= @vc_hash["computes"][0]["vc_host"] %>')
-$vsphere_login     = inline_template('<%= @vc_hash["computes"][0]["vc_user"] %>')
-$vsphere_password  = inline_template('<%= @vc_hash["computes"][0]["vc_password"] %>')
-$dvs_network_maps  = inline_template('<%= @dvs_hash["vmware_dvs_net_maps"] %>')
-$neutron_physnet   = inline_template('<%= @neutron_hash["predefined_networks"]["net04"]["L2"]["physnet"] %>')
+$vc_hash               = hiera('vcenter', {})
+$dvs_hash              = hiera('fuel-plugin-vmware-dvs', {})
+$neutron_hash          = hiera('quantum_settings', {})
+$nets                  = $neutron_hash['predefined_networks']
+$vsphere_hostname      = inline_template('<%= @vc_hash["computes"][0]["vc_host"] %>')
+$vsphere_login         = inline_template('<%= @vc_hash["computes"][0]["vc_user"] %>')
+$vsphere_password      = inline_template('<%= @vc_hash["computes"][0]["vc_password"] %>')
+$dvs_network_maps      = inline_template('<%= @dvs_hash["vmware_dvs_net_maps"] %>')
+$neutron_physnet       = inline_template('<%= @neutron_hash["predefined_networks"]["net04"]["L2"]["physnet"] %>')
+$primary_controller    = hiera('primary_controller')
+$access_hash           = hiera('access', {})
+$keystone_admin_tenant = $access_hash[tenant]
 
 class {'vmware_dvs':
-  vsphere_hostname => $vsphere_hostname,
-  vsphere_login    => $vsphere_login,
-  vsphere_password => $vsphere_password,
-  network_maps     => $dvs_network_maps,
-  neutron_physnet  => $neutron_physnet,
-  driver_name      => 'vmware_dvs'
+  vsphere_hostname      => $vsphere_hostname,
+  vsphere_login         => $vsphere_login,
+  vsphere_password      => $vsphere_password,
+  network_maps          => $dvs_network_maps,
+  neutron_physnet       => $neutron_physnet,
+  primary_controller    => $primary_controller,
+  nets                  => $nets,
+  keystone_admin_tenant => $keystone_admin_tenant,
+  driver_name           => 'vmware_dvs',
 }
