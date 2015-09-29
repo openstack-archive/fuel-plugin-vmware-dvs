691ec5052f592e9bf1eef94465a6d6aeba181f91
Oleksandr Martsyniuk <omartsyniuk@mirantis.com>
2015-09-29 19:35:55 +0300

    Changes for Fuel 7.0 and FPB 3.0
    - plugin metadata updated
    - nailgun patch routines added
    - neutron puppet manifest updated
    
    Change-Id: I4b09c2ebd78f9759cf9d76fa7d90d6d06a2cf91d

--- deployment_scripts/puppet/modules/vmware_dvs/manifests/init.pp
+++ deployment_scripts/puppet/modules/vmware_dvs/manifests/init.pp
@@ -33,21 +33,36 @@ class vmware_dvs(
   $keystone_admin_tenant,
   $driver_name         = 'vmware_dvs',
   $neutron_url_timeout = '3600',
+  $primary_controller  = hiera('primary_controller')
 )
 {
   $true_network_maps = get_network_maps($network_maps, $neutron_physnet)
 
+  package {['python-pip','python-dev','git-core']:
+    ensure => present,
+  } ->
+  package {'suds':
+    ensure   => latest,
+    provider => 'pip',
+    source   => 'git+git://github.com/yunesj/suds.git',
+  } ->
+  package {'mech-vmware-dvs':
+    ensure   => present,
+    provider => 'pip',
+    source   => 'git+git://github.com/Mirantis/vmware-dvs.git',
+  }
+
   neutron_config {
     'DEFAULT/notification_driver': value => 'messagingv2';
     'DEFAULT/notification_topics': value => 'notifications,vmware_dvs';
-  }
+  } ->
 
   neutron_plugin_ml2 {
     'ml2_vmware/vsphere_hostname': value => $vsphere_hostname;
     'ml2_vmware/vsphere_login':    value => $vsphere_login;
     'ml2_vmware/vsphere_password': value => $vsphere_password;
     'ml2_vmware/network_maps':     value => $true_network_maps;
-  }
+  } ->
 
   ini_subsetting {'vmware_dvs_driver':
     path                 => '/etc/neutron/plugin.ini',
@@ -55,27 +70,43 @@ class vmware_dvs(
     setting              => 'mechanism_drivers',
     subsetting           => $driver_name,
     subsetting_separator => ','
-    } ->
-    exec {'restart_neutron':
-      command => 'service neutron-server restart',
-      path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
     }
 
-    Service<| title == 'neutron-server' |> ->
-    Openstack::Network::Create_network <||>
+  file_line { 'neutron_timeout':
+    path  => '/etc/haproxy/conf.d/085-neutron.cfg',
+    line  => '  timeout server 1h',
+    after => 'listen neutron',
+  }
 
-    Service<| title == 'neutron-server' |> ->
-    Openstack::Network::Create_router <||>
+  service { 'neutron-server':
+    ensure      => running,
+    enable      => true,
+    subscribe   => [Package['mech-vmware-dvs'],Ini_Subsetting['vmware_dvs_driver']],
+  }
+
+  service {'haproxy':
+    ensure     => running,
+    hasrestart => true,
+    restart    => '/sbin/ip netns exec haproxy service haproxy reload',
+    subscribe  => File_Line['neutron_timeout'],
+  }
+
+  if $primary_controller and $nets and !empty($nets) {
 
     openstack::network::create_network{'net04':
-      netdata => $nets['net04']
-      } ->
-      openstack::network::create_network{'net04_ext':
-        netdata => $nets['net04_ext']
-        } ->
-        openstack::network::create_router{'router04':
-          internal_network => 'net04',
-          external_network => 'net04_ext',
-          tenant_name      => $keystone_admin_tenant
-        }
+      netdata           => $nets['net04'],
+      segmentation_type => 'vlan',
+      require           => Service['neutron-server'],
+    } ->
+    openstack::network::create_network{'net04_ext':
+      netdata           => $nets['net04_ext'],
+      segmentation_type => 'local',
+    } ->
+    openstack::network::create_router{'router04':
+      internal_network => 'net04',
+      external_network => 'net04_ext',
+      tenant_name      => $keystone_admin_tenant
+    }
+  }
+
 }
