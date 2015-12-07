2b03ea1d3bebd8db84534b8a9bb5976a8986e02c
otsvigun <otsvigun@mirantis.com>
2015-12-07 23:52:05 +0200

    Add system tests
    Change-Id: Idbedf6723a4c87656abf1e0a99b38016539c96fa

--- plugin_test/run_tests.py
+++ plugin_test/run_tests.py
@@ -42,7 +42,12 @@ class CloseSSHConnectionsPlugin(Plugin):
 
 
 def import_tests():
-    from tests import test_plugin_vmware_dvs
+    from tests import test_plugin_vmware_dvs_integration
+    from tests import test_plugin_vmware_dvs_smoke
+    from tests import test_plugin_vmware_dvs_system
+    from tests import test_plugin_vmware_dvs_destructive
+    from tests import test_plugin_vmware_dvs_net_template
+    from tests import test_plugin_vmware_dvs_maintenance
 
 
 def run_tests():
