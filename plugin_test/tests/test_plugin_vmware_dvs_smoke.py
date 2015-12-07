436343f91e67b0d86f4e92694e342fbf2cf26d3c
otsvigun <otsvigun@mirantis.com>
2015-12-07 23:52:05 +0200

    Update and add new system tests.
    -added new tests:
     dvs_vcenter_security
     dvs_vcenter_tenants_isolation
     dvs_vcenter_same_ip
     dvs_vcenter_maintenance
     dvs_vcenter_bind_port
    -updated structure of test suites according test plan
    
    Change-Id: Idbedf6723a4c87656abf1e0a99b38016539c96fa

---
+++ plugin_test/tests/test_plugin_vmware_dvs_smoke.py
@@ -0,0 +1,129 @@
+#    Copyright 2014 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+from proboscis import test
+
+from fuelweb_test.helpers.decorators import log_snapshot_after_test
+from fuelweb_test.settings import DEPLOYMENT_MODE
+from fuelweb_test.settings import NEUTRON_SEGMENT_TYPE
+from fuelweb_test.tests.base_test_case import SetupEnvironment
+from fuelweb_test.tests.base_test_case import TestBasic
+
+from helpers import plugin
+
+
+@test(groups=["plugins", 'dvs_vcenter_plugin'])
+class TestDVSPlugin(TestBasic):
+
+    # constants
+    node_name = lambda self, name_node: self.fuel_web. \
+        get_nailgun_node_by_name(name_node)['hostname']
+
+    @test(depends_on=[SetupEnvironment.prepare_slaves_3],
+          groups=["dvs_vcenter_smoke", "dvs_vcenter_plugin"])
+    @log_snapshot_after_test
+    def dvs_vcenter_smoke(self):
+        """Deploy cluster with plugin and vmware datastore backend
+
+        Scenario:
+            1. Upload plugins to the master node
+            2. Install plugin.
+            3. Create cluster with vcenter.
+            4. Add 1 node with controller role.
+            5. Deploy the cluster.
+            6. Run OSTF.
+
+        Duration 1.8 hours
+
+        """
+        self.env.revert_snapshot("ready_with_3_slaves")
+
+        plugin.install_dvs_plugin(self.env.d_env.get_admin_remote())
+
+        # Configure cluster with 2 vcenter clusters
+        cluster_id = self.fuel_web.create_cluster(
+            name=self.__class__.__name__,
+            mode=DEPLOYMENT_MODE,
+            settings={
+                "net_provider": 'neutron',
+                "net_segment_type": NEUTRON_SEGMENT_TYPE
+            }
+        )
+        plugin.enable_plugin(cluster_id, self.fuel_web)
+
+        # Assign role to node
+        self.fuel_web.update_nodes(
+            cluster_id,
+            {'slave-01': ['controller']}
+        )
+
+        # Configure VMWare vCenter settings
+        self.fuel_web.vcenter_configure(cluster_id)
+
+        self.fuel_web.deploy_cluster_wait(cluster_id)
+
+        self.fuel_web.run_ostf(
+            cluster_id=cluster_id, test_sets=['smoke'])
+
+    @test(depends_on=[SetupEnvironment.prepare_slaves_3],
+          groups=["dvs_vcenter_bvt", "dvs_vcenter_plugin"])
+    @log_snapshot_after_test
+    def dvs_vcenter_bvt(self):
+        """Deploy cluster with plugin and vmware datastore backend
+
+        Scenario:
+            1. Upload plugins to the master node
+            2. Install plugin.
+            3. Create cluster with vcenter.
+            4. Add 1 node with controller role.
+            5. Add 1 node with compute role.
+            6. Deploy the cluster.
+            7. Run OSTF.
+
+        Duration 1.8 hours
+
+        """
+        self.env.revert_snapshot("ready_with_3_slaves")
+
+        plugin.install_dvs_plugin(self.env.d_env.get_admin_remote())
+
+        # Configure cluster with 2 vcenter clusters and vcenter glance
+        cluster_id = self.fuel_web.create_cluster(
+            name=self.__class__.__name__,
+            mode=DEPLOYMENT_MODE,
+            settings={
+                "net_provider": 'neutron',
+                "net_segment_type": NEUTRON_SEGMENT_TYPE,
+                'images_vcenter': True
+            }
+        )
+        plugin.install_dvs_plugin(self.env.d_env.get_admin_remote())
+
+        # Assign role to node
+        self.fuel_web.update_nodes(
+            cluster_id,
+            {'slave-01': ['controller'],
+             'slave-02': ['compute'], }
+        )
+
+        # Configure VMWare vCenter settings
+        self.fuel_web.vcenter_configure(
+            cluster_id,
+            multiclusters=True,
+            vc_glance=True
+        )
+
+        self.fuel_web.deploy_cluster_wait(cluster_id)
+
+        self.fuel_web.run_ostf(
+            cluster_id=cluster_id, test_sets=['smoke'])
