46f482b553aab414ef8235b9d318c704e8389a04
otsvigun <otsvigun@mirantis.com>
2015-12-10 16:29:34 +0200

    Update and add new system tests.
    -added new tests:
     dvs_vcenter_security
     dvs_vcenter_tenants_isolation
     dvs_vcenter_same_ip
     dvs_vcenter_maintenance
     dvs_vcenter_bind_port
    -updated structure of test suites according test plan.
    
    Change-Id: I3b89255618ba231f4cc907ef21b743ee9e2a16f6

---
+++ plugin_test/helpers/plugin.py
@@ -0,0 +1,53 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import os
+
+
+from proboscis.asserts import assert_true
+
+
+from fuelweb_test.helpers import checkers
+from fuelweb_test import logger
+
+
+# constants
+DVS_PLUGIN_PATH = os.environ.get('DVS_PLUGIN_PATH')
+plugin_name = 'fuel-plugin-vmware-dvs'
+msg = "Plugin couldn't be enabled. Check plugin version. Test aborted"
+dvs_switch_name = ['dvSwitch']
+
+
+def install_dvs_plugin(master_node):
+    # copy plugins to the master node
+    checkers.upload_tarball(
+        master_node,
+        DVS_PLUGIN_PATH, "/var")
+
+    # install plugin
+    checkers.install_plugin_check_code(
+        master_node,
+        plugin=os.path.basename(DVS_PLUGIN_PATH))
+
+
+def enable_plugin(cluster_id, fuel_web_client):
+    assert_true(
+        fuel_web_client.check_plugin_exists(cluster_id, plugin_name),
+        msg)
+    options = {'metadata/enabled': True,
+               '#1_vmware_dvs_net_maps/value': dvs_switch_name[0]}
+
+    fuel_web_client.update_plugin_data(cluster_id, plugin_name, options)
+
+    logger.info("cluster is {}".format(cluster_id))
