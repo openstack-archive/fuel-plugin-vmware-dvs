7b1d32773b378aaaf533e2deceb38c154117f78f
otsvigun <otsvigun@mirantis.com>
2016-03-03 17:08:33 +0200

    Add test dvs_different_networks and doc fix.
    -add test dvs_different_networks
    -update test dvs_different_networks in Test Plan
    -update doc strings
    -update test dvs_regression
    -fix rally heat
    
    Change-Id: I41c4d9f8de2356540a7974c55c8cf74c2859a6e2

--- plugin_test/tests/test_plugin_vmware_dvs_smoke.py
+++ plugin_test/tests/test_plugin_vmware_dvs_smoke.py
@@ -1,33 +1,41 @@
-#    Copyright 2014 Mirantis, Inc.
-#
-#    Licensed under the Apache License, Version 2.0 (the "License"); you may
-#    not use this file except in compliance with the License. You may obtain
-#    a copy of the License at
-#
-#         http://www.apache.org/licenses/LICENSE-2.0
-#
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-#    License for the specific language governing permissions and limitations
-#    under the License.
-from proboscis import test
+"""Copyright 2016 Mirantis, Inc.
 
+Licensed under the Apache License, Version 2.0 (the "License"); you may
+not use this file except in compliance with the License. You may obtain
+copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+License for the specific language governing permissions and limitations
+under the License.
+"""
 from fuelweb_test.helpers.decorators import log_snapshot_after_test
+
 from fuelweb_test.settings import DEPLOYMENT_MODE
 from fuelweb_test.settings import NEUTRON_SEGMENT_TYPE
+
 from fuelweb_test.tests.base_test_case import SetupEnvironment
 from fuelweb_test.tests.base_test_case import TestBasic
 
 from helpers import plugin
 
+from proboscis import test
+
 
 @test(groups=["plugins", 'dvs_vcenter_plugin'])
-class TestDVSPlugin(TestBasic):
+class TestDVSSmoke(TestBasic):
 
-    # constants
-    node_name = lambda self, name_node: self.fuel_web. \
-        get_nailgun_node_by_name(name_node)['hostname']
+    """The goal of smoke testing is to ensure that the most critical features
+    of Fuel VMware DVS plugin work  after new build delivery. Smoke tests
+    will be used by QA to accept software builds from Development team.
+    """
+
+    def node_name(self, name_node):
+        """Get node by name."""
+        return self.fuel_web.get_nailgun_node_by_name(name_node)['hostname']
 
     @test(depends_on=[SetupEnvironment.prepare_slaves_3],
           groups=["dvs_vcenter_smoke", "dvs_vcenter_plugin"])
@@ -54,7 +62,7 @@ class TestDVSPlugin(TestBasic):
             10. Deploy the cluster.
             11. Run OSTF.
 
-        Duration 1.8 hours
+        Duration: 1.8 hours
 
         """
         self.env.revert_snapshot("ready_with_3_slaves")
@@ -122,7 +130,7 @@ class TestDVSPlugin(TestBasic):
             9. Deploy the cluster.
             10. Run OSTF.
 
-        Duration 1.8 hours
+        Duration: 1.8 hours
 
         """
         self.env.revert_snapshot("ready_with_9_slaves")
