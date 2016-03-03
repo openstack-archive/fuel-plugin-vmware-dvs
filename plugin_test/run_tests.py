03c91eb052f65bd39df48da80207f3a9d052f0fc
otsvigun <otsvigun@mirantis.com>
2016-03-03 17:08:33 +0200

    Add test dvs_different_networks and doc fix.
    -add test dvs_different_networks
    -update test dvs_different_networks in Test Plan
    -update doc strings
    -update test dvs_regression
    -fix rally heat
    -fix flake8 H306, I100
    
    Change-Id: I41c4d9f8de2356540a7974c55c8cf74c2859a6e2

--- plugin_test/run_tests.py
+++ plugin_test/run_tests.py
@@ -1,54 +1,65 @@
-#    Copyright 2015 Mirantis, Inc.
-#
-#    Licensed under the Apache License, Version 2.0 (the "License"); you may
-#    not use this file except in compliance with the License. You may obtain
-#    a copy of the License at
-#
-#         http://www.apache.org/licenses/LICENSE-2.0
-#
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-#    License for the specific language governing permissions and limitations
-#    under the License.
+#!/usr/bin/env python
+"""Copyright 2016 Mirantis, Inc.
 
-import sys
+Licensed under the Apache License, Version 2.0 (the "License"); you may
+not use this file except in compliance with the License. You may obtain
+copy of the License at
 
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+License for the specific language governing permissions and limitations
+under the License.
+"""
 
 import os
 import re
+import sys
+
 from nose.plugins import Plugin
+
 from paramiko.transport import _join_lingering_threads
 
 
 class CloseSSHConnectionsPlugin(Plugin):
-    """Closes all paramiko's ssh connections after each test case
+    """Closes all paramiko's ssh connections after each test case.
 
     Plugin fixes proboscis disability to run cleanup of any kind.
-    'afterTest' calls _join_lingering_threads function from paramiko,
-    which stops all threads (set the state to inactive and joins for 10s)
     """
+
     name = 'closesshconnections'
 
     def options(self, parser, env=os.environ):
+        """Options."""
         super(CloseSSHConnectionsPlugin, self).options(parser, env=env)
 
     def configure(self, options, conf):
+        """Configure env."""
         super(CloseSSHConnectionsPlugin, self).configure(options, conf)
         self.enabled = True
 
-    def afterTest(self, *args, **kwargs):
+    def after_test(self, *args, **kwargs):
+        """After_Test.
+
+        After_Test calls _join_lingering_threads function from paramiko,
+        which stops all threads (set the state to inactive and joins for 10s).
+        """
         _join_lingering_threads()
 
 
 def import_tests():
-    from tests import test_plugin_vmware_dvs_destructive
-    from tests import test_plugin_vmware_dvs_maintenance
-    from tests import test_plugin_vmware_dvs_smoke
-    from tests import test_plugin_vmware_dvs_system
-    from tests import test_plugin_vmware_dvs_templates
+    """Import test suite of project."""
+    from tests import test_plugin_vmware_dvs_destructive  # noqa
+    from tests import test_plugin_vmware_dvs_maintenance  # noqa
+    from tests import test_plugin_vmware_dvs_smoke  # noqa
+    from tests import test_plugin_vmware_dvs_system  # noqa
+    from tests import test_plugin_vmware_dvs_templates  # noqa
+
 
 def run_tests():
+    """Run test cases."""
     from proboscis import TestProgram  # noqa
 
     # Check if the specified test group starts any test case
@@ -64,7 +75,7 @@ def run_tests():
 
 
 if __name__ == '__main__':
-    sys.path.append(sys.path[0]+"/fuel-qa")
+    sys.path.append(sys.path[0] + "/fuel-qa")
     import_tests()
     from fuelweb_test.helpers.patching import map_test
     if any(re.search(r'--group=patching_master_tests', arg)
