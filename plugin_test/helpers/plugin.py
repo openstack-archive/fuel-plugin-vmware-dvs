10d338f43b3e4bb2b7c2df0289b1fbb4642f6218
otsvigun <otsvigun@mirantis.com>
2016-04-07 22:16:47 +0300

    Remove dvs mapping and add tests.
    Change-Id: Ibdf30f22105cc6851867458d8e20da4cbc16a9cd

--- plugin_test/helpers/plugin.py
+++ plugin_test/helpers/plugin.py
@@ -15,8 +15,6 @@ under the License.
 
 import os
 
-from fuelweb_test import logger
-
 from fuelweb_test.helpers import checkers
 
 from proboscis.asserts import assert_true
@@ -36,28 +34,19 @@ def install_dvs_plugin(master_node):
     checkers.upload_tarball(
         master_node,
         DVS_PLUGIN_PATH, "/var")
-
     # install plugin
     checkers.install_plugin_check_code(
         master_node,
         plugin=os.path.basename(DVS_PLUGIN_PATH))
 
 
-def enable_plugin(cluster_id, fuel_web_client, multiclusters=True):
+def enable_plugin(cluster_id, fuel_web_client):
     """Enable DVS plugin on cluster."""
     assert_true(
         fuel_web_client.check_plugin_exists(
             cluster_id, plugin_name),
         msg)
-    map_switch_clusters = map(
-        lambda x, y: "{0}:{1}".format(x, y), VCENTER_CLUSTERS, DVS_SWITCHES)
-    if multiclusters is True:
-        options = {'vmware_dvs_net_maps/value': ';'.join(map_switch_clusters)}
-    else:
-        options = {'vmware_dvs_net_maps/value': map_switch_clusters[0]}
-
-    logger.info("cluster is {0}".format(cluster_id))
-
-    fuel_web_client.update_plugin_settings(
-        cluster_id, plugin_name,
-        DVS_PLUGIN_VERSION, options)
+    cluster_attr = fuel_web_client.client.get_cluster_attributes(cluster_id)
+    plugin_data = cluster_attr['editable'][plugin_name]
+    plugin_data['metadata']['enabled'] = True
+    fuel_web_client.client.update_cluster_attributes(cluster_id, cluster_attr)
